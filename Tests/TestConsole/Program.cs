bb7570b8d9c8d5ccc238ece495d4fcde43ed3f92
Natan11 <phartynov@mail.ru>
2020-02-21 22:27:25 +0700

    Наклепали сотню игроков

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace TestConsole
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            var gamer = new Gamer();
+            gamer.Name = "Игрок 1";
+            gamer.DayOfBirth = new DateTime(1978, 12, 26);
+
+            Gamer[] gamers = new Gamer[100];
+            for(int i = 0; i < gamers.Length; i++)
+            {
+                var g = new Gamer();
+                g.Name = $"Gamer {i + 1}";
+                g.DayOfBirth = DateTime.Now;
+                gamers[i] = g;
+            }
+            Console.ReadLine();
         }
+    }
+    class Gamer
+    {
+        public string Name;
+        public DateTime DayOfBirth;
     }
 }
