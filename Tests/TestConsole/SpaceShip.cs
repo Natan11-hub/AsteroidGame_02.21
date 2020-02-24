524b2d9a26e594ae09bfc02d65d1767440e3b7d9
Natan11 <phartynov@mail.ru>
2020-02-24 18:29:01 +0700

    Описание космического корабля и двумерного вектора его положения

---
+++ Tests/TestConsole/SpaceShip.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class SpaceShip
+    {
+        private Vector2D _Position = new Vector2D(5, 7);
+
+        public SpaceShip()
+        {
+
+        }
+
+        public SpaceShip(Vector2D Position)
+        {
+            _Position = Position;
+        }
+    }
+
+    struct Vector2D
+    {
+        private double _X;
+        private double _Y;
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Length => Math.Sqrt(X * X + Y * Y);
+
+        public Vector2D(double X, double Y)
+        {
+            _X = X;
+            _Y = Y;
+        }
+    }
+}
