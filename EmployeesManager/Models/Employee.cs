62b953a98ebdd95fceb32e36eda4a8d70eca30cd
Natan11 <phartynov@mail.ru>
2020-03-14 13:42:32 +0700

    Элементы MVVM

---
+++ EmployeesManager/Models/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesManager.Models
+{
+    class Employee : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public int Id { get; set; }
+
+        private string _Name { get; set; }
+
+        public string Name
+        {
+            get => Name;
+            set
+            {
+                _Name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+            }
+        }
+
+        public string SurName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public DateTime DayOfBirth { get; set; }
+
+        public int Age => (int)Math.Floor((DateTime.Now - DayOfBirth).TotalDays / 365);
+
+
+        public override string ToString() => $"Сотрудник[{Id}]:{SurName}";
+    }
+}
