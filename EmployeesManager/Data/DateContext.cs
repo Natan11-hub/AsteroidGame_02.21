030c9cb314f342cf70dd6a54f40d84f4bbba02f7
Natan11 <phartynov@mail.ru>
2020-03-17 13:15:10 +0700

    ДЗ

---
+++ EmployeesManager/Data/DateContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Entity;
+using EmployeesManager.Models;
+
+namespace EmployeesManager.Data
+{
+    class DataContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Departament> Departaments { get; set; }
+
+        public DataContext(string ConnectionString) : base(ConnectionString)
+        {
+
+        }
+
+        public DataContext() : this("Name=DefConnection")
+        {
+        }
+    }
+}
